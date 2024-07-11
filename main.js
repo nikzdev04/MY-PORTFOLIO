2a97960e26c9fa34c7162989e11cf4db232340b3
Nikhil Kumar <nikzdev04@gmail.com>
2024-07-12 00:54:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Developer","Graphical Designer","Prompt Engineer",,"JAVA Developer","Python Enthusiast"],
+    typeSpeed:200,
+    backSpeed:200,
+    backDelay:500, 
+    loop:true
+});
